e252d44f9ba38a1ec4d94bc512ef34e4684b037d
Acoidan Martín Conrado <alu0101623627@ull.edu.es>
2025-01-03 11:58:12 +0000

    sudoku resuelto

--- src/Tema-7/Hoja-de-problemas/sudoku/src/sudoku_solver.cc
+++ src/Tema-7/Hoja-de-problemas/sudoku/src/sudoku_solver.cc
@@ -1,9 +1,22 @@
-#include <iostream>
-#include <cmath>
+bool Sudoku::solve(void) {
+  short row, col;
+  
+  // Si no hay celdas libres, sudoku resuelto
+  if (!find_empty_cell(row, col)) {
+    return true;
+  }
 
-int main() {
-  int i = std::pow(2,2); 
-  std::cout << i << "\n";
-  return 0;
+  for (short num = 1; num <= N; num++) {
+    if (check_all(row, col, num)) {
+      m_(row, col) = num;
+
+      // Si solve devuelve true, encontró solución y se propaga hacia atrás.
+      if (solve()) return true;
+    }
+
+    // No se puede rellenar celdas
+    m_(row, col) = 0; // vacío
+    return false;
+  }
 }
 
